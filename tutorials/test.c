d635f8af0a82009fd513276ef0e64028deabc3fb
Nathaniel96 <lawalolusegun96@gmail.com>
2023-09-19 17:56:24 +0100

    test is here

---
+++ tutorials/test.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - adding integers 
+ * @a: parameter checked
+ * @b: parameter checked
+ * Return: 0
+ */
+
+int main(int a, int b)
+{
+	int a = 5;
+	int b = 7;
+
+	printf(a + b);
+}
+
